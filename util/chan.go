a1600ca75be20f61c803b523459d0c0deceda0d2
Denis <admin@doubleds.ru>
2026-03-24 11:33:12 +0100

    ReadFromChannel, ReadFromChannelWithTimeout

---
+++ util/chan.go
@@ -0,0 +1,43 @@
+package util
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+var (
+	ErrContext       = errors.New("context")
+	ErrChannelClosed = errors.New("channel closed")
+)
+
+func ReadFromChannelWithTimeout[T any](ctx context.Context, ch <-chan T, timeout time.Duration, defaultValue T) (T, error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	result, err := ReadFromChannel(ctx, ch, defaultValue)
+
+	switch {
+	case errors.Is(err, context.DeadlineExceeded):
+		return result, nil
+
+	default:
+		return result, err
+	}
+}
+
+func ReadFromChannel[T any](ctx context.Context, ch <-chan T, defaultValue T) (T, error) {
+	select {
+	case <-ctx.Done():
+		err := ctx.Err()
+
+		return defaultValue, errors.Join(ErrContext, err)
+
+	case v, ok := <-ch:
+		if !ok {
+			return defaultValue, ErrChannelClosed
+		}
+
+		return v, nil
+	}
+}
---
+++ util/chan_test.go
@@ -0,0 +1,79 @@
+package util
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ReadFromChannel(t *testing.T) {
+	t.Parallel()
+
+	ch := make(chan int, 1)
+	ch <- 1
+
+	result, err := ReadFromChannel(context.Background(), ch, 0)
+	require.NoError(t, err)
+	require.Equal(t, 1, result)
+}
+
+func Test_ReadFromChannel_Timeout(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+
+	ch := make(chan int, 1)
+
+	t.Cleanup(func() {
+		cancel()
+	})
+
+	result, err := ReadFromChannel(ctx, ch, 0)
+	require.ErrorIs(t, err, ErrContext)
+	require.Equal(t, 0, result)
+}
+
+func Test_ReadFromChannel_Closed(t *testing.T) {
+	t.Parallel()
+
+	ch := make(chan int, 1)
+	close(ch)
+
+	result, err := ReadFromChannel(context.Background(), ch, 0)
+	require.ErrorIs(t, err, ErrChannelClosed)
+	require.Equal(t, 0, result)
+}
+
+func Test_ReadFromChannelWithTimeout(t *testing.T) {
+	t.Parallel()
+
+	ch := make(chan int, 1)
+	ch <- 1
+
+	result, err := ReadFromChannelWithTimeout(context.Background(), ch, 100*time.Millisecond, 0)
+	require.NoError(t, err)
+	require.Equal(t, 1, result)
+}
+
+func Test_ReadFromChannelWithTimeout_Timeout(t *testing.T) {
+	t.Parallel()
+
+	ch := make(chan int, 1)
+
+	result, err := ReadFromChannelWithTimeout(context.Background(), ch, 100*time.Millisecond, 0)
+	require.NoError(t, err)
+	require.Equal(t, 0, result)
+}
+
+func Test_ReadFromChannelWithTimeout_Closed(t *testing.T) {
+	t.Parallel()
+
+	ch := make(chan int, 1)
+	close(ch)
+
+	result, err := ReadFromChannelWithTimeout(context.Background(), ch, 100*time.Millisecond, 0)
+	require.ErrorIs(t, err, ErrChannelClosed)
+	require.Equal(t, 0, result)
+}
